61028ce1206b21f77a1b42f76a1311e28d952d38
ishanbatra09 <107885395+ishanbatra09@users.noreply.github.com>
2023-05-22 23:01:59 +0530

    update about page

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -3,7 +3,7 @@ const Aboutpage = () => {
     <div className="about">
       <h3 className="about__title">ABOUT PAGE</h3>
       <div className="about__info">
-        <p>Tv Show Search App (v1.0)</p>
+        <p>Movie Show Search App (v1.0)</p>
         <div>
           <p>Things learnt from app build:</p>
           <ul>
